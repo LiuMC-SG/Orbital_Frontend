c63e70ff74c782c842ff8e76a01dbd5ee301bea1
Liu Muchen <liumuchen1@gmail.com>
2022-05-12 19:41:18 +0800

    (Fix): Remove unused imports

--- helpus/lib/widgets/profile/profile_photo_edit.dart
+++ helpus/lib/widgets/profile/profile_photo_edit.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:helpus/utilities/constants.dart';
 import 'package:helpus/widgets/profile/profile_photo.dart';
